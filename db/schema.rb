668e779756c69315f072e96097fd24b616111410
Norman Institorisz <normaninstitorisz11@gmail.com>
2020-01-14 20:52:52 +0000

    finished blog comments feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_173750) do
+ActiveRecord::Schema.define(version: 2020_01_14_202106) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,4 +19,24 @@ ActiveRecord::Schema.define(version: 2019_12_19_173750) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "c_omments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_c_omments_on_article_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "c_omments", "articles"
+  add_foreign_key "comments", "articles"
 end
